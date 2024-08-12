7d45066a7aede908e5038651406d6b8b6d365c92
sandflexx <eporn627@gmail.com>
2024-08-12 21:46:52 +0100

    Update file.lua

--- file.lua
+++ file.lua
@@ -9,8 +9,19 @@ local Tab = Window:MakeTab({
 })
 
 local Section = Tab:AddSection({
-	Name = "Script 1: Infinite Yield"
+	Name = "Script 1"
 })
+
+Tab:AddButton({
+	Name = "Infinite Yield",
+	Callback = loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+  	end    
+})
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
 
 
 
