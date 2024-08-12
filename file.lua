e756dc0c06e5da8c11ad491f4c54e18d3d599362
sandflexx <eporn627@gmail.com>
2024-08-12 21:39:14 +0100

    Update file.lua

--- file.lua
+++ file.lua
@@ -1,1 +1,10 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Gengi Script Hub", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest", IntroEnabled = true, IntroText = "Welcome to Gengi"})
+
+local Tab = Window:MakeTab({
+	Name = "Universal",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
